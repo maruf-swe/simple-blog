ec103e49e28e72e7295bfe4309ce33e4ceb3d762
Maruf <marufur1847@gmail.com>
2021-07-27 21:32:52 +0600

    add all files

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,11 @@
 from django.shortcuts import render
-
-# Create your views here.
-posts = [
-    {'author': 'maruf',
-     'title': 'Blog Post 1',
-     'content': 'First post content',
-     'date_posted': 'August 27, 2018'
-     },
-    {'author': 'robel',
-     'title': 'Blog Post 2',
-     'content': 'second post content',
-     'date_posted': 'August 27, 2018'
-     }
-]
+from .models import Post
 
 
 def home(request):
     context = {
         'title': 'Home',
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
