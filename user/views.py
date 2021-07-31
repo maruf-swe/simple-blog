da74f29547a5e73bede8cdd85ec9066575a40584
Maruf <marufur1847@gmail.com>
2021-08-01 00:48:31 +0600

    User Register form add

--- user/views.py
+++ user/views.py
@@ -1,13 +1,20 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 from django.contrib import messages
+from .forms import UserRegisterForm
+
 
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account Created for {username}')
+            return redirect('blog-home')
     else:
-        form = UserCreationForm()
-    return render(request, 'user/register.html', {'form': form})
+        form = UserRegisterForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, 'user/register.html', context)
