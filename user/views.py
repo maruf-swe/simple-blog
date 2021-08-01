a5ce47b1f1a6e81ed40a23a4f3d1367b39486ba6
Maruf <marufur1847@gmail.com>
2021-08-01 19:43:32 +0600

    add signals functionally

--- user/views.py
+++ user/views.py
@@ -11,7 +11,7 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account Created for {username}')
-            return redirect('blog-home')
+            return redirect('login')
     else:
         form = UserRegisterForm()
 
