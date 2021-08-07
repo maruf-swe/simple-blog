a72ac741064064a9292f55169674df2c62616294
Maruf <marufur1847@gmail.com>
2021-08-07 22:41:30 +0600

    Add password reset complete

--- django_project/urls.py
+++ django_project/urls.py
@@ -4,20 +4,22 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('user/', include('user.urls')),
     path('password-reset/',
          auth_views.PasswordResetView.as_view(template_name='user/password_reset.html'),
-         name='password-reset'),
+         name='password_reset'),
     path('password-reset/done/',
          auth_views.PasswordResetDoneView.as_view(template_name='user/password_reset_done.html'),
          name='password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>/',
          auth_views.PasswordResetConfirmView.as_view(template_name='user/password_reset_confirm.html'),
          name='password_reset_confirm'),
+    path('password-reset-complete',
+         auth_views.PasswordResetCompleteView.as_view(template_name='user/password_reset_complete.html'),
+         name='password_reset_complete'),
 ]
 
 if settings.DEBUG:
