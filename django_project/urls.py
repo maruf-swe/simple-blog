e3822fd28ff97672e46273fd348a9c8f47258d25
Maruf <marufur1847@gmail.com>
2021-08-07 22:34:57 +0600

    Add password reset confirm

--- django_project/urls.py
+++ django_project/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
@@ -14,7 +15,7 @@ urlpatterns = [
     path('password-reset/done/',
          auth_views.PasswordResetDoneView.as_view(template_name='user/password_reset_done.html'),
          name='password_reset_done'),
-    path('password-reset-confirm/<uid64>/<token>/',
+    path('password-reset-confirm/<uidb64>/<token>/',
          auth_views.PasswordResetConfirmView.as_view(template_name='user/password_reset_confirm.html'),
          name='password_reset_confirm'),
 ]
